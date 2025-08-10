2aad934ea816cc0a75b12e309a4ec81d3454f580
DanD <152017997+danieldrinhausen@users.noreply.github.com>
2025-08-10 12:32:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/vue-resume-template/',
-    plugins: [vue()],
-    css: {
-        preprocessorOptions: {
-            scss: {
-                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
-            },
-        },
-    }
+  plugins: [vue()],
+  base: '/', // deploy at domain root
 })
